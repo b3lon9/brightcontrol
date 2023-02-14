732087094304025d5155a58391d230e756340fd4
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-02-14 09:17:04 +0900

    widget base setting

--- app/src/main/java/com/b3lon9/app/brightcontrol/MainActivity.java
+++ app/src/main/java/com/b3lon9/app/brightcontrol/MainActivity.java
@@ -1,17 +1,27 @@
 package com.b3lon9.app.brightcontrol;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // <<< naver api contents
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.7f;
+        getWindow().setAttributes(layoutParams);
+        // >>>
+
         setContentView(R.layout.activity_main);
 
         TextView label = this.findViewById(R.id.label);
@@ -27,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                     value = 100;
                 }
 
-                label.setText(value);
+                //label.setText(value);
             }
 
             @Override

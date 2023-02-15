60b171157ff070018eeccbcb6eab48a3f0bd4a09
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-02-15 14:29:04 +0900

    팝업 윈도우 화면 재조정
    - 디스플레이 화면의 80% 크기로 표출되도록 하기
    - 팝업 윈도우 위치를 움직일 수 있는가?
    - 이미지가 xml에 표시된 것 처럼 나오게하자

--- app/src/main/java/com/b3lon9/app/brightcontrol/MainActivity.java
+++ app/src/main/java/com/b3lon9/app/brightcontrol/MainActivity.java
@@ -2,8 +2,11 @@ package com.b3lon9.app.brightcontrol;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,12 +16,18 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        int widthPixels = getResources().getDisplayMetrics().widthPixels;  // 1080
+        int heightPixels = getResources().getDisplayMetrics().heightPixels; // 2401
+        Log.d("neander", "display widthPixels : " + widthPixels);
+        Log.d("neander", "display heightPixels : " + heightPixels);
+        Log.d("neader", "display density : " + getResources().getDisplayMetrics().density);
         // <<< naver api contents
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        layoutParams.dimAmount = 0.7f;
+        layoutParams.gravity = Gravity.CENTER;
+        // layoutParams.width = (int)(widthPixels * 0.5);
+        // layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+//        layoutParams.dimAmount = 0.7f;
         getWindow().setAttributes(layoutParams);
         // >>>
 
@@ -26,6 +35,10 @@ public class MainActivity extends Activity {
 
         TextView label = this.findViewById(R.id.label);
         SeekBar seekBar = this.findViewById(R.id.seekbar);
+         LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams1.width = (int)(widthPixels * 0.8);
+        seekBar.setLayoutParams(layoutParams1);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
